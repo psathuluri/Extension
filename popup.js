610aff1203324e374af6086b41cec10a5d9bf08d
Paul Sathuluri <psathuluri@utexas.edu>
2019-11-09 02:19:45 -0600

    aesthetic changes

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     // your button here
     var link = document.getElementById('btnOpenNewTab');
     var link2 = document.getElementById('btnSee');
+    var link3 = document.getElementById('btnCourse');
     // onClick's logic below:
     link.addEventListener('click', function() {
         var newURL = "http://avacadra.com/";
@@ -11,6 +12,12 @@ window.addEventListener('DOMContentLoaded', function() {
         var newURL = "https://utdirect.utexas.edu/registrar/waitlist/wl_see_my_waitlists.WBX";
         chrome.tabs.create({ url: newURL });
     });
+    link3.addEventListener('click', function() {
+        var newURL = "https://registrar.utexas.edu/schedules";
+        chrome.tabs.create({ url: newURL });
+    });
+
+    
 });
 
 
